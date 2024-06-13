6967a374f4d196c05d93a4c7a86b25cb486d46da
Satyam Subhash Sawant <satyamsawant54@gmail.com>
2024-06-13 12:28:11 +0530

    clean the Trending.js file

--- src/Pages/Trending/Trending.js
+++ src/Pages/Trending/Trending.js
@@ -1,21 +1,6 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 const Trending = () => {
-  const [content, setContent] = useState([]);
-
-  const fetchTrending = async () => {
-    const { data } = await axios.get(
-      `https://api.themoviedb.org/3/trending/all/day?api_key=${process.env.REACT_APP_API_KEY}`
-    );
-    console.log(data);
-    setContent(data.results);
-  };
-
-  useEffect(() => {
-    fetchTrending();
-  }, []);
-
   return (
     <div>
       <span className="pageTitle">Trending </span>
