f746cb8da7fd9dc59501bdb790a6fcc91fa278ae
Satyam Subhash Sawant <satyamsawant54@gmail.com>
2024-06-13 09:38:59 +0530

    modify Header.js

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import Header from "./components/Header/Header";
 
 function App() {
   return (
-    <div className="App">
+    <>
       <Header />
-    </div>
+      <div className="app">MOVIES</div>
+    </>
   );
 }
 
