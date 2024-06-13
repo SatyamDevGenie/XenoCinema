a956b090a8cc4e1f42dd34a005f1ff90a5e8f6e0
Satyam Subhash Sawant <satyamsawant54@gmail.com>
2024-06-13 10:07:29 +0530

    created a bottom navigation component using MUI

--- src/components/MainNav.js
+++ src/components/MainNav.js
@@ -1,27 +1,51 @@
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import LocationOnIcon from "@mui/icons-material/LocationOn";
-import RestoreIcon from "@mui/icons-material/Restore";
+import MovieIcon from "@mui/icons-material/Movie";
+import SearchIcon from "@mui/icons-material/Search";
+import TvIcon from "@mui/icons-material/Tv";
+import WhatshotIcon from "@mui/icons-material/Whatshot";
 import BottomNavigation from "@mui/material/BottomNavigation";
 import BottomNavigationAction from "@mui/material/BottomNavigationAction";
-import Box from "@mui/material/Box";
-import * as React from "react";
+import { styled } from "@mui/system";
+import React from "react";
+
+const StyledBottomNavigation = styled(BottomNavigation)({
+  width: "100%",
+  position: "fixed",
+  bottom: 0,
+  backgroundColor: "#2d313a",
+  zIndex: 100,
+});
 
 export default function SimpleBottomNavigation() {
   const [value, setValue] = React.useState(0);
 
   return (
-    <Box sx={{ width: 500 }}>
-      <BottomNavigation
-        showLabels
-        value={value}
-        onChange={(event, newValue) => {
-          setValue(newValue);
-        }}
-      >
-        <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
-        <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
-        <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
-      </BottomNavigation>
-    </Box>
+    <StyledBottomNavigation
+      value={value}
+      onChange={(event, newValue) => {
+        setValue(newValue);
+      }}
+      showLabels
+    >
+      <BottomNavigationAction
+        label="Trending"
+        icon={<WhatshotIcon />}
+        style={{ color: "wheat" }}
+      />
+      <BottomNavigationAction
+        label="Movies"
+        icon={<MovieIcon />}
+        style={{ color: "wheat" }}
+      />
+      <BottomNavigationAction
+        label="TV Series"
+        icon={<TvIcon />}
+        style={{ color: "wheat" }}
+      />
+      <BottomNavigationAction
+        label="Search"
+        icon={<SearchIcon />}
+        style={{ color: "wheat" }}
+      />
+    </StyledBottomNavigation>
   );
 }
