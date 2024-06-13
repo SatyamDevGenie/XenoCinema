6b6275965f876ba48f936ab5ab3e0f830a74d769
Satyam Subhash Sawant <satyamsawant54@gmail.com>
2024-06-13 09:06:02 +0530

    clean App.css file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <span>Header</span>
+    </div>
+  );
 }
 
 export default App;
