189f62ac107ba425fef22c2c9e6a726b012eb3ad
Satyam Subhash Sawant <satyamsawant54@gmail.com>
2024-06-13 09:46:45 +0530

    created a simple bottom navigation bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Header from "./components/Header/Header";
+import SimpleBottomNavigation from "./components/MainNav";
 
 function App() {
   return (
     <>
       <Header />
       <div className="app">MOVIES</div>
+      <SimpleBottomNavigation />
     </>
   );
 }
---
+++ src/components/MainNav.js
@@ -0,0 +1,27 @@
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import RestoreIcon from "@mui/icons-material/Restore";
+import BottomNavigation from "@mui/material/BottomNavigation";
+import BottomNavigationAction from "@mui/material/BottomNavigationAction";
+import Box from "@mui/material/Box";
+import * as React from "react";
+
+export default function SimpleBottomNavigation() {
+  const [value, setValue] = React.useState(0);
+
+  return (
+    <Box sx={{ width: 500 }}>
+      <BottomNavigation
+        showLabels
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+      >
+        <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
+        <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
+        <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+      </BottomNavigation>
+    </Box>
+  );
+}
