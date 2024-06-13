92fb4cbee8726f48973dd6fa09c6542bc30cee7b
Satyam Subhash Sawant <satyamsawant54@gmail.com>
2024-06-13 20:21:27 +0530

    data fetched correctly

--- src/components/SingleContent/SingleContent.js
+++ src/components/SingleContent/SingleContent.js
@@ -1,7 +1,14 @@
 import React from "react";
 
-const SingleContent = () => {
-  return <div>SingleContent</div>;
+const SingleContent = ({
+  id,
+  poster,
+  title,
+  date,
+  media_type,
+  vote_average,
+}) => {
+  return <div>{title}</div>;
 };
 
 export default SingleContent;
