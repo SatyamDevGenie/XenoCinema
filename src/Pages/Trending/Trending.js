2648edddb5d734bbd9cd3ae052e2420e1e210b01
Satyam Subhash Sawant <satyamsawant54@gmail.com>
2024-06-13 12:13:23 +0530

    Added .gitignore to ignore .env files and node_modules directory

--- src/Pages/Trending/Trending.js
+++ src/Pages/Trending/Trending.js
@@ -3,7 +3,7 @@ import React from "react";
 const Trending = () => {
   return (
     <div>
-      <span className="pageTitle">Trending</span>
+      <span className="pageTitle">TRENDING</span>
     </div>
   );
 };
