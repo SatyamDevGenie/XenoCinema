153670241548f700c0f0c10aecc288a04bf5c2fc
Satyam Subhash Sawant <satyamsawant54@gmail.com>
2024-06-18 23:30:05 +0530

    pagination is working on Movies page

--- src/Pages/Movies/Movies.js
+++ src/Pages/Movies/Movies.js
@@ -20,7 +20,7 @@ const Movies = () => {
 
   useEffect(() => {
     fetchMovies();
-  }, []);
+  }, [page]);
 
   return (
     <div>
@@ -34,7 +34,7 @@ const Movies = () => {
               poster={c.poster_path}
               title={c.title || c.name}
               date={c.first_air_date || c.release_date}
-              media_type={c.media_type}
+              media_type="movie"
               vote_average={c.vote_average}
             />
           ))}
