00d64c50c80680d133b8be93a6762c9be82bb68c
Satyam Subhash Sawant <satyamsawant54@gmail.com>
2024-06-19 00:33:57 +0530

    remove genre functionalit done

--- src/components/Genres/Genres.js
+++ src/components/Genres/Genres.js
@@ -61,6 +61,14 @@ const Genres = ({
     setPage(1);
   };
 
+  const handleRemove = (genre) => {
+    setSelectedGenres(
+      selectedGenres.filter((selected) => selected.id !== genre.id)
+    );
+    setGenres([...genres, genre]);
+    setPage(1);
+  };
+
   const fetchGenres = async () => {
     const { data } = await axios.get(
       `https://api.themoviedb.org/3/genre/${type}/list?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`
@@ -77,6 +85,17 @@ const Genres = ({
 
   return (
     <div style={{ padding: "8px 0", margin: "12" }}>
+      {selectedGenres.map((genre) => (
+        <Chip
+          style={{ margin: 2 }}
+          label={genre.name}
+          key={genre.id}
+          color="primary"
+          clickable
+          size="small"
+          onDelete={() => handleRemove(genre)}
+        />
+      ))}
       {genres.map((genre) => (
         <Chip
           style={{ backgroundColor: "white" }}
