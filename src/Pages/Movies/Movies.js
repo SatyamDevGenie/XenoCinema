15f9fbe78e318e32cd1090dd6534725e3ef80e36
Satyam Subhash Sawant <satyamsawant54@gmail.com>
2024-06-19 00:50:21 +0530

    added a custom hook for GENRES

--- src/Pages/Movies/Movies.js
+++ src/Pages/Movies/Movies.js
@@ -4,8 +4,8 @@ import React, { useEffect, useState } from "react";
 import CustomPagination from "../../components/Pagination/CustomPagiation";
 import SingleContent from "../../components/SingleContent/SingleContent";
 
-// import Genres from "../../components/Genres/Genres";
 import Genres from "../../components/Genres/Genres";
+import useGenre from "../../hooks/useGenre";
 
 const Movies = () => {
   const [page, setPage] = useState(1);
@@ -13,10 +13,11 @@ const Movies = () => {
   const [numOfPages, setNumOfPages] = useState();
   const [genres, setGenres] = useState([]);
   const [selectedGenres, setSelectedGenres] = useState([]);
+  const genreforURL = useGenre(selectedGenres);
 
   const fetchMovies = async () => {
     const { data } = await axios.get(
-      `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}`
+      `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}&with_genres=${genreforURL}`
     );
 
     console.log(data);
