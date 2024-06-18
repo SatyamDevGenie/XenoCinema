4a4df2e37a5f84d5274040903689abbba0d4d560
Satyam Subhash Sawant <satyamsawant54@gmail.com>
2024-06-19 00:22:46 +0530

    Genres implemented

--- src/components/Genres/Genres.js
+++ src/components/Genres/Genres.js
@@ -1,3 +1,49 @@
+// import Chip from "@mui/material/Chip";
+// import axios from "axios";
+// import { useEffect } from "react";
+
+// const Genres = ({
+//   selectedGenres,
+//   setSelectedGenres,
+//   genres,
+//   setGenres,
+//   type,
+//   setPage,
+// }) => {
+//   const fetchGenres = async () => {
+//     const { data } = await axios.get(
+//       `https://api.themoviedb.org/3/genre/${type}/list?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`
+//     );
+//     setGenres(data.genres);
+//   };
+
+//   console.log(genres);
+
+//   useEffect(() => {
+//     fetchGenres();
+//     return () => {
+//       setGenres({}); // unmounting
+//     };
+//   }, []);
+
+//   return (
+//     <div style={{ padding: "6px 0" }}>
+//       {genres.map((genre) => (
+//         <Chip
+//           style={{ margin: 2 }}
+//           label={genre.name}
+//           key={genre.id}
+//           clickable
+//           size="small"
+//         />
+//       ))}
+//     </div>
+//   );
+// };
+
+// export default Genres;
+
+import Chip from "@mui/material/Chip";
 import axios from "axios";
 import { useEffect } from "react";
 
@@ -16,16 +62,26 @@ const Genres = ({
     setGenres(data.genres);
   };
 
-  console.log(genres);
-
   useEffect(() => {
     fetchGenres();
     return () => {
-      setGenres({}); // unmounting
+      setGenres([]); // unmounting, set to empty array
     };
   }, []);
 
-  return <div style={{ padding: "6px 0" }}>Genres</div>;
+  return (
+    <div style={{ padding: "6px 0" }}>
+      {genres.map((genre) => (
+        <Chip
+          style={{ margin: 2 }}
+          label={genre.name}
+          key={genre.id}
+          clickable
+          size="small"
+        />
+      ))}
+    </div>
+  );
 };
 
 export default Genres;
