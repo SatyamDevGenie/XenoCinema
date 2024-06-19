82b2c06efca30b0eb7644fb6e45199706ac6e7a4
Satyam Subhash Sawant <satyamsawant54@gmail.com>
2024-06-19 21:27:23 +0530

    add basic css to search input

--- src/Pages/Searching/Searching.js
+++ src/Pages/Searching/Searching.js
@@ -1,4 +1,6 @@
-import { createTheme } from "@mui/material/styles";
+import { Button } from "@mui/base";
+import SearchIcon from "@mui/icons-material/Search";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import TextField from "@mui/material/TextField";
 import React, { useState } from "react";
 
@@ -16,14 +18,21 @@ const Searching = () => {
 
   return (
     <div>
-      <TextField
-        style={{ flex: 1 }}
-        className="searchBox"
-        label="Search"
-        // variant="filled"
-        variant="outlined"
-        // onChange={(e) => setSearchText(e.target.value)}
-      />
+      <ThemeProvider theme={darkTheme}>
+        <div style={{ display: "flex", margin: "15px 0" }}>
+          <TextField
+            style={{ flex: 1 }}
+            className="searchBox"
+            label="Search"
+            // variant="filled"
+            variant="outlined"
+            // onChange={(e) => setSearchText(e.target.value)}
+          />
+          <Button variant="conatinaed" style={{ marginLeft: "10" }}>
+            <SearchIcon />
+          </Button>
+        </div>
+      </ThemeProvider>
     </div>
   );
 };
