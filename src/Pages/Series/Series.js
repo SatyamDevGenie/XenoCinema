9a2f78de098bd08d5c3855a61bd871a0607a71a4
Satyam Subhash Sawant <satyamsawant54@gmail.com>
2024-06-19 21:01:52 +0530

    Movies.js some code pasted in Series.js

--- src/Pages/Series/Series.js
+++ src/Pages/Series/Series.js
@@ -1,9 +1,62 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Genres from "../../components/Genres/Genres";
+import CustomPagination from "../../components/Pagination/CustomPagiation";
+import SingleContent from "../../components/SingleContent/SingleContent";
+import useGenre from "../../hooks/useGenre";
 
 const Series = () => {
+  const [page, setPage] = useState(1);
+  const [content, setContent] = useState([]);
+  const [numOfPages, setNumOfPages] = useState();
+  const [genres, setGenres] = useState([]);
+  const [selectedGenres, setSelectedGenres] = useState([]);
+  const genreforURL = useGenre(selectedGenres);
+
+  const fetchMovies = async () => {
+    const { data } = await axios.get(
+      `https://api.themoviedb.org/3/discover/tv?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}&with_genres=${genreforURL}`
+    );
+
+    console.log(data);
+    setContent(data.results);
+    setNumOfPages(data.total_pages);
+  };
+
+  useEffect(() => {
+    fetchMovies();
+  }, [page, genreforURL]);
+
   return (
     <div>
       <span className="pageTitle">TV Series</span>
+      <Genres
+        type="tv"
+        selectedGenres={selectedGenres}
+        setSelectedGenres={setSelectedGenres}
+        genres={genres}
+        setGenres={setGenres}
+        setPage={setPage}
+      />
+      <div className="trending">
+        {content &&
+          content.map((c) => (
+            <SingleContent
+              key={c.id}
+              id={c.id}
+              poster={c.poster_path}
+              title={c.title || c.name}
+              date={c.first_air_date || c.release_date}
+              media_type="movie"
+              vote_average={c.vote_average}
+            />
+          ))}
+      </div>
+      <br /> <br />
+      {numOfPages > 1 && (
+        <CustomPagination setPage={setPage} numOfPages={numOfPages} /> // bug is there on numOfPages
+      )}
+      <br /> <br />
     </div>
   );
 };
